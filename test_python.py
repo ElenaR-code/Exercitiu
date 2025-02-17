86ddfb2cd46c6dd4b11215daf89c899731dc7559
Elena Raghina <elena.raghina@gmail.com>
2025-02-17 22:16:09 +0200

    Corectare eroare din linia 3

--- test_python.py
+++ test_python.py
@@ -1,6 +1,6 @@
 someNumber = 10 # modificare pentru a putea adauga fisierul in staging
 
-if(someNumber <= 0):
+if(someNumber > 0):
     print("Over zerro")
 elif(someNumber < 0):
     print("Bellow zerro")
